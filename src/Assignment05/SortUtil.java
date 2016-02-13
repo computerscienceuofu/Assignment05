8dcf3aedbf5a39ee440619ec070cfcd4aa0616f8
chris <u0190322@utah.edu>
2016-02-13 13:08:39 -0700

    5 methods

--- src/Assignment05/SortUtil.java
+++ src/Assignment05/SortUtil.java
@@ -1,5 +1,42 @@
 package Assignment05;
+import java.util.*;
 
 public class SortUtil {
+	
+	private static int threshold = 6;
+	
+	
+	public static <T> void mergesort(ArrayList<T>, Comparator<? super T>)
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			
+		}
+	}
+	
+	public static <T> void quicksort(ArrayList<T>, Comparator<? super T>)
+	{
+		
+	}
+	
+	
+	//This method generates and returns an ArrayList of integers 1 to size in ascending order.
+	public static ArrayList<Integer> generateBestCase(int size){
+		
+	}
+
+	
+	//This method generates and returns an ArrayList of integers 1 to size in permuted order (i,e., randomly ordered). 
+	public static ArrayList<Integer> generateAverageCase(int size){
+		
+	}
+
+	
+	//This method generates and returns an ArrayList of integers 1 to size in descending order.
+	public static ArrayList<Integer> generateWorstCase(int size){
+		
+	}
+	
+
 
 }
