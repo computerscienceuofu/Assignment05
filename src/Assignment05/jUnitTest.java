5b42eb834b500a420e375615b7ae32f1e8c188c4
chris <u0190322@utah.edu>
2016-02-13 13:19:23 -0700

    jUnit Test Class

---
+++ src/Assignment05/jUnitTest.java
@@ -0,0 +1,14 @@
+package Assignment05;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class jUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
