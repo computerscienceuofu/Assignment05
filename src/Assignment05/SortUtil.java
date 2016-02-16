43dacfbcb29d0e4d90b6f3232f74b3c9fc222256
chris <u0190322@utah.edu>
2016-02-15 21:18:11 -0700

    merge

--- src/Assignment05/SortUtil.java
+++ src/Assignment05/SortUtil.java
@@ -1,11 +1,43 @@
 package Assignment05;
 import java.util.*;
 
-public class SortUtil {
+public class SortUtil <T>{
 	
-	private static int threshold = 6;
+	private static int threshold = 4;	
+	ArrayList<T> dataSet;
+	ArrayList<T> temp;
 	
-    /**
+	
+	
+	public static <T> void insertionSort(ArrayList<T> dataSet, int left, int right, Comparator<? super T> Comparator)
+	{
+		if(dataSet == null || Comparator == null)
+			return;
+		
+		for (int i = 0; i < dataSet.size(); i++) {
+			T itemToBeSorted = dataSet.get(i);
+			if(itemToBeSorted == null)
+				continue;
+			int sortIndex = i - 1;
+
+			while (sortIndex >= 0) {
+
+				if (dataSet.get(sortIndex) == null || Comparator.compare(itemToBeSorted, dataSet.get(sortIndex)) < 0) {
+					dataSet.set(sortIndex + 1, dataSet.get(sortIndex));
+					dataSet.set(sortIndex, itemToBeSorted);
+					sortIndex--;
+				} else {
+					dataSet.set(sortIndex + 1, itemToBeSorted);
+					break;
+				}
+			}
+
+		}
+		
+	}
+
+
+	/**
      * This method performs a mergesort on the generic ArrayList given as input.
      * For the mergesort algorithm, see the class notes and/or the textbook. 
      * There is pseudo code in the slides. Your mergesort implementation must switch over 
@@ -18,9 +50,75 @@ public class SortUtil {
     */
 	public static <T> void mergesort(ArrayList<T> dataSet, Comparator<? super T> Comparator)
 	{
-
+		
+	//if (dataSet.size() == 0)
+	//		 throw new NoSuchElementException();
+		int left = 0;
+		int right = dataSet.size();
+		
+		mergesort(dataSet, left, right, Comparator);
 		
 	}
+	
+	private static <T> void mergesort(ArrayList<T> dataSet, int left, int right, Comparator<? super T> Comparator)
+	{
+		if (left < right)
+		{
+			int middle = (left + right) / 2;
+			mergesort(dataSet, left, middle, Comparator);
+			mergesort(dataSet, middle + 1, right, Comparator);
+			merge(dataSet, left, middle, right, Comparator);
+		}
+	}
+
+
+	private static <T> void merge(ArrayList<T> dataSet, int left, int mid, int right, Comparator<? super T> Comparator)
+	{
+				
+		int h, i, j, k;
+		ArrayList<T> temp = new ArrayList<T>();
+		h = left;
+		i = left;
+		j = mid +1;
+		
+		while ((h <= mid) && (j <= right))
+		{
+			if(Comparator.compare(dataSet.get(h), dataSet.get(j)) <= 0)
+			{
+				temp.set(i, dataSet.get(h));
+				h++;
+			}
+			else
+			{
+				temp.set(i, dataSet.get(j));
+				j++;
+			}
+			i++;
+		}
+		if (h > mid)
+		{
+			for(k = j; k <= right; k++)
+			{
+				temp.set(i, dataSet.get(k));
+				i++;
+			}
+		}
+		else
+		{
+			for(k = h; k <= mid; k++)
+			{
+				temp.set(i, dataSet.get(k));
+				i++;
+			}
+		}
+		for (k = left; k < right; k++)
+		{
+			dataSet.set(k, temp.get(k));
+		}
+	}
+
+	
+	
 	
 	
 	
@@ -43,38 +141,14 @@ public class SortUtil {
 		
 	}
 	
+
 	
 	/**This is the insertionSort.  It will be used only when the other searches hit the item threshold.
 	 * 
 	 * @param sortAnagram
 	 * @param comparator
 	 */
-	public static <T> void insertionSort(T[] sortAnagram, Comparator<? super T> comparator) {
-
-		if(sortAnagram == null || comparator == null)
-			return;
-		
-		for (int i = 0; i < sortAnagram.length; i++) {
-			T itemToBeSorted = sortAnagram[i];
-			if(itemToBeSorted == null)
-				continue;
-			int sortIndex = i - 1;
-
-			while (sortIndex >= 0) {
-
-				if (sortAnagram[sortIndex] == null || comparator.compare(itemToBeSorted, sortAnagram[sortIndex]) < 0) {
-					sortAnagram[sortIndex + 1] = sortAnagram[sortIndex];
-					sortAnagram[sortIndex] = itemToBeSorted;
-					sortIndex--;
-				} else {
-					sortAnagram[sortIndex + 1] = itemToBeSorted;
-					break;
-				}
-			}
-
-		}
-
-	}
+	
 	
 	
 	/**This method generates and returns an ArrayList of integers 1 to size in ascending order.
@@ -84,7 +158,11 @@ public class SortUtil {
 	 */
 	public static ArrayList<Integer> generateBestCase(int size)
 	{
-		return null;
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		for (int i = 0; i < size; i++)
+			temp.add(i);
+		
+		return temp;
 		
 	}
 
@@ -92,11 +170,16 @@ public class SortUtil {
 	/**This method generates and returns an ArrayList of integers 1 to size in permuted order (i,e., randomly ordered).
 	 *  
 	 * @param size
-	 * @return
+	 * @return temp
 	 */
 	public static ArrayList<Integer> generateAverageCase(int size)
 	{
-		return null;
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		for (int i = 0; i < size; i++)
+			temp.add(i);
+		for (int i = 0; i < size; i++)
+			swap(temp, i, (int)(Math.random() * size));
+		return temp;	
 		
 	}
 
@@ -104,14 +187,33 @@ public class SortUtil {
 	/**This method generates and returns an ArrayList of integers 1 to size in descending order.
 	 * 
 	 * @param size
-	 * @return
+	 * @return 
 	 */
 	public static ArrayList<Integer> generateWorstCase(int size)
 	{
-		return null;
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		for(int i = size-1; i >= 0; i--)
+			temp.add(i);
+		return temp;
 		
 	}
 	
+	/**This method swaps two elements
+	 * @param <T>
+	 * 
+	 * @param array
+	 * @param first
+	 * @param second
+	 */
+	public static <T> void swap(ArrayList<T> array, int first, int second)
+	{
+		T temp = array.get(first);
+		array.set(first, array.get(second));
+		array.set(second, temp);
+		
+	}
 
+	
+	
 
 }
--- src/Assignment05/jUnitTest.java
+++ src/Assignment05/jUnitTest.java
@@ -1,14 +1,51 @@
 package Assignment05;
-
+import java.util.*;
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class jUnitTest {
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test(){
+		
+		Comparator<Integer> comp = new Comparator<Integer>() {
+		    public int compare(Integer i1, Integer i2) {
+		    	return i1 - i2;
+		    }
+		};
+		
+		ArrayList<Integer> temp = SortUtil.generateBestCase(10);
+		ArrayList<Integer> temp2 = SortUtil.generateWorstCase(10);
+		ArrayList<Integer> temp3 = SortUtil.generateAverageCase(10);
+				
+	//	SortUtil.mergesort(temp, comp);
+		SortUtil.mergesort(temp2, comp);
+	//	SortUtil.mergesort(temp3, comp);
+	//	for(int i = 0; i < temp.size(); i++)
+	//	{
+	//		System.out.println(temp.get(i));
+	//		
+	//	}
+		for(int i = 0; i < temp2.size(); i++)
+		{
+			System.out.println(temp2.get(i));
+			
+		}
+	//	for(int i = 0; i < temp3.size(); i++)
+	//	{
+	//		System.out.println(temp3.get(i));
+	//		
+	//	}
+		
+		
+		
+
+		
+		
+		
+		
+		
 	}
 
 }
